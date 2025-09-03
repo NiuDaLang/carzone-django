abac1853d084754570c505b8894b22f684f48ec2
NiuDaLang <gogocfa@yahoo.co.jp>
2025-09-03 23:38:59 +1000

    complete teams model

--- carzone/settings.py
+++ carzone/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "carzone",
+    "pages",
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = "carzone.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "carzone",
+        "USER": "postgres",
+        "PASSWORD": "CHIQUITA",
+        "HOST": "localhost",
     }
 }
 
@@ -125,3 +129,7 @@ STATIC_FILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Media Settings
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR /"media" 
